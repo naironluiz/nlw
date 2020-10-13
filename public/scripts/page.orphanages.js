092613854a8c811bfd34b838be9c60c5d2aee545
Nairon Luiz <nairon.luiz@gmail.com>
2020-10-13 11:53:20 -0300

    animações e js

--- public/scripts/page.orphanages.js
+++ public/scripts/page.orphanages.js
@@ -1,1 +1,30 @@
-let mymap = L.map('mapid').setView([51.505, -0.09], 13);
+//create map
+const map = L.map('mapid').setView([-15.8114666,-47.9907921], 15);
+
+// create and add tileLayer
+L.tileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+    //create icon
+
+    const icon = L.icon({
+        iconUrl:"./public/images/map-marker.svg",
+        iconSize: [58,68],
+        iconAnchor: [29,68],
+        popupAnchor: [170,2]
+    })
+
+    //create popuo overlay
+
+    const popup = L.popup({
+        closeButton: false,
+        className: 'map-popup',
+        minWidth: 240,
+        minHeight: 240
+    }).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"</a>')
+
+    //create and add marker
+L.marker([-15.8114666,-47.9907921], {icon})
+    .addTo(map)
+    .bindPopup(popup)
+    .openPopup();
